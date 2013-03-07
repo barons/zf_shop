1bb3ab076616586ac5b6638be046049fc9126306
barons <samuel@baronsdesign.be>
2013-03-07 19:10:06 +0100

    laatste versie

---
+++ htdocs/application/forms/Login.php
@@ -0,0 +1,40 @@
+<?php
+
+// altijd woorden in enkelvoud
+class Application_Form_Login extends Zend_Form
+{
+
+    public function init()
+    {
+        
+        $this->setMethod(Zend_Form::METHOD_POST);
+        $this->setAttrib('enctype', Zend_Form::ENCTYPE_MULTIPART);
+        // <form method="post" enctype="multipart/form-data">
+        
+        // titel
+        $this->addElement(new Zend_Form_Element_Text('titel', array(
+            'label'     => 'Titel',
+            'filters'   => array('StringTrim'),
+            'required'  => true
+        )));
+        
+        // omschrijving
+        $this->addElement(new Zend_Form_Element_Textarea('omschrijving', array(
+            'label'     => 'Omschrijving',
+            'filters'   => array('StringTrim'),
+            'required'  => true
+        )));
+        
+        // verzenden
+        // submit is hetzelfde maar niet de keuze zoals button
+        $this->addElement(new Zend_Form_Element_Button('versturen', array(
+            'type'      => 'submit',
+            'label'     => 'Nieuws toevoegen',
+            'required'  => false,
+            'ignore'    => true
+        )));
+                
+        
+    }
+    
+}
